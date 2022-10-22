066d4ebd6f2d9a58a06aca6e505545f78ba28081
Bruno Corrao <103332103+imnotseneca@users.noreply.github.com>
2022-10-22 13:09:54 -0300

    Transform string to Uppercase

---
+++ Javascript/fundamentals/MakeUpperCase.js
@@ -0,0 +1,12 @@
+//Write a function which converts the input string to uppercase.
+
+//Function declaration.
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+
+//Arrow function
+
+const makeUpperCase = str => str.toUpperCase();
