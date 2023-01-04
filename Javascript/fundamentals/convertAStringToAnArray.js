e9ffd23d889172b29c233247a3fdc096c6a3f1b6
Bruno Corrao <103332103+imnotseneca@users.noreply.github.com>
2023-01-04 01:28:48 -0300

    convert a string to an array

---
+++ Javascript/fundamentals/convertAStringToAnArray.js
@@ -0,0 +1,9 @@
+//PREP:
+/*
+-PARAMETERS: we receive "string" as string of words.
+-RETURN: we must return an array of words correlated to "string"
+-EXAMPLES: "Robin Singh" ==> ["Robin", "Singh"]
+-PSEUDO-CODE:
+*/
+
+const stringToArray = string => string.split(' ')
