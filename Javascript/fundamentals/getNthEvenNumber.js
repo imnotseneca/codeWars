93e77329843cfc4ec3385ca026a6aece0aafa755
BrunoC <corraobruno13@gmail.com>
2023-04-04 08:54:25 -0300

    daily codewars challenge with PREP method

---
+++ Javascript/fundamentals/getNthEvenNumber.js
@@ -0,0 +1,15 @@
+//PARAMETERS: we receive "n" as a INTEGER.
+//RETURN: we must return the n-th Even Number equivalent.
+//EXAMPLE: 
+/*
+1 --> 0 (the first even number is 0)
+3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+100 --> 198
+1298734 --> 2597466
+*/
+//PSEUDO-CODE: 
+
+
+function nthEven(n){
+  return n * 2 - 2
+  }
