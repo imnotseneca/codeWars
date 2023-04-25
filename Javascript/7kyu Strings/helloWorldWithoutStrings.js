04f15cc96d02419a99c320743313da4176e838f8
BrunoC <corraobruno13@gmail.com>
2023-04-25 14:12:12 -0300

    added 7jyu strings directory plus 7kyu string challenge

---
+++ Javascript/7kyu Strings/helloWorldWithoutStrings.js
@@ -0,0 +1,17 @@
+
+
+
+/*
+You need to create a function, helloWorld, that will return the String Hello, World! without actually using raw strings. This includes quotes, double quotes and template strings. You can, however, use the String constructor and any related functions.
+
+You cannot use the following:
+
+"Hello, World!"
+'Hello, World!'
+`Hello, World!`
+Good luck and try to be as creative as possible!
+
+*/
+const helloWorld = () => String.fromCharCode(72,101,108,108,111,44,32,87,111,114,108,100,33)
+
+//Alternatively called the character set, charset, and character encoding, a character code describes a specific encoding for characters as defined in the code page. Each character code defines how the bits in a text stream are mapped to the characters they represent.
